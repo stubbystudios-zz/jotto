1b47fd881337b14b023af41dabc6be67eaf27da3
Carey Best <careybest@Careys-MBP.attlocal.net>
2020-10-11 17:59:17 -0500

    Tests and code for non-zero guessed words

---
+++ src/GuessedWords/GuessedWords.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GuessedWords = (props) => {
+  let contents;
+  if (props.guessedWords.length === 0) {
+    contents = (
+      <p
+        data-test='guess-instructions'
+        className='guess-instructions'
+      >
+        Try to guess the secret word!
+      </p>
+    );
+  } else {
+    const guessedWordsRows = props.guessedWords.map((word, index) => (
+      <div
+        data-test='guessed-word'
+        className='guessed-word'
+        key={index}
+      >
+        <div className='guess-word-result guess-word-col'>{word.guessedWord}</div>
+        <div className='guess-word-result matching-letter-col'>{word.letterMatchCount}</div>
+      </div>
+    ));
+    contents = (
+      <section
+        data-test='guessed-words'
+        className='guessed-words'
+      >
+        <header>
+          <h3>Guessed Words</h3>
+        </header>
+        <div className='guessed-words-list'>
+          <div className='guess-word-header guess-word-col'>Guess</div>
+          <div className='guess-word-header matching-letter-col'>Matching Letters</div>
+          {guessedWordsRows}
+        </div>
+      </section>
+    )
+  }
+  return (
+    <section
+      data-test='component-guessed-words'
+      className='component-guessed-words'
+    >{contents}</section>
+  );
+};
+
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default GuessedWords;
