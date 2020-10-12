6d006e61203db8aa3d1e03bf7da4d0c34f58b14f
Carey Best <careybest@Careys-MBP.attlocal.net>
2020-10-12 18:12:12 -0500

    Complete Input component render tests for success

--- src/Input/Input.jsx
+++ src/Input/Input.jsx
@@ -3,16 +3,34 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
   render() {
+    const contents = this.props.success
+      ? null : (
+        <form className='form'>
+          <input
+            data-test='input-box'
+            data-className='input-box'
+            type='text'
+            placeholder='enter guess'
+          />
+          <button
+            data-test='submit-button'
+            className='submit-button'
+            type='submit'
+          >
+            Submit
+          </button>
+        </form>
+      )
     return (
-      <section>
-        <button></button>
+      <section data-test='component-input' className='component-input'>
+        {contents}
       </section>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 }
 
 export default connect(mapStateToProps)(Input);
